51db23e194812d1ea69bb113b01c8b075316d00d
William Pettersson <william@ewpettersson.se>
2019-01-15 10:09:32 +0000

    PEP8: fix spacing between functions

--- tests/test_max_card_matching.py
+++ tests/test_max_card_matching.py
@@ -6,11 +6,13 @@ from nose.tools import eq_, raises
 from pyhrtc.instance import read_hrtc
 from pyhrtc.algorithms import max_card_matching
 
+
 def test_max_card_matching_simple():
     """test1 is a simple instance of SMTI.
     """
     instance = read_hrtc("tests/testfiles/test1.instance")
     eq_(max_card_matching(instance), 2)
+
 
 def test_max_card_matching_smti():
     """This is a much larger SMTI instance.
@@ -18,11 +20,13 @@ def test_max_card_matching_smti():
     instance = read_hrtc("tests/testfiles/smti.instance")
     eq_(max_card_matching(instance), 9941)
 
+
 def test_max_card_matching_hrt():
     """An instance with capacities.
     """
     instance = read_hrtc("tests/testfiles/hrt.instance")
     eq_(max_card_matching(instance), 759)
+
 
 @raises(Exception)
 def test_max_card_matching_hrct():
