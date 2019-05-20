9d82ff8f9b524a2c08914dba16974d084c4c6f6f
William Pettersson <william@ewpettersson.se>
2019-05-20 17:04:39 +0100

    New version to update website

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyhrtc",
-    version="0.0.1",
+    version="0.0.2",
     author="William Pettersson",
     author_email="william@ewpettersson.se",
     description="Various algorithms and utilities for stable matching problems",
