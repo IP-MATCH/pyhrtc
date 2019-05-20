6953a73b371d934e66a81cb520365174b8a6ec49
William Pettersson <william.pettersson@glasgow.ac.uk>
2019-05-20 15:05:22 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyhrtc",
+    version="0.0.1",
+    author="William Pettersson",
+    author_email="william@ewpettersson.se",
+    description="Various algorithms and utilities for stable matching problems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wpettersson/pyhrtc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "LICENSE :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+    ],
+)
