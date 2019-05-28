0f3078a6fdb7e83b1d4e02d80290eb385f2e4473
William Pettersson <william@ewpettersson.se>
2019-05-28 17:59:50 +0100

    More thorough testing.

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -6,6 +6,7 @@ from nose.tools import eq_
 from pyhrtc.fileio import read_hrtc
 from pyhrtc.models import MAX_SMTI_IP
 
+
 def test_solve():
     """Solve simple instance
     """
@@ -17,3 +18,27 @@ def test_solve():
     model = MAX_SMTI_IP(instance)
     matching = model.solve()
     eq_(len(matching), 2)
+
+
+def test_dummy_variables_simple():
+    """Solve simple instance using dummy variables
+    """
+    instance = read_hrtc("tests/testfiles/smti-simple.instance")
+    model = MAX_SMTI_IP(instance)
+    model.dummy_variables = True
+    matching = model.solve()
+    eq_(len(matching), 3)
+
+
+def test_grp_solve():
+    """Solve GRP instances
+    """
+    instance = read_hrtc("tests/testfiles/smti-grp-simple.instance")
+    model = MAX_SMTI_IP(instance)
+    matching = model.solve()
+    eq_(len(matching), 3)
+    instance = read_hrtc("tests/testfiles/smti-grp-simple.instance")
+    model = MAX_SMTI_IP(instance)
+    model.dummy_variables = True
+    matching = model.solve()
+    eq_(len(matching), 3)
