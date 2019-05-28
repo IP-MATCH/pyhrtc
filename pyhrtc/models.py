2cf5f12bf81f0a6d22f93e9ee99b84e20a258a3e
William Pettersson <william@ewpettersson.se>
2019-05-28 17:59:04 +0100

    Add dummy variables to IP model

--- pyhrtc/models.py
+++ pyhrtc/models.py
@@ -2,8 +2,12 @@
 
 from pulp import LpProblem, LpMaximize, LpVariable, lpSum, LpStatusOptimal
 
+from pyhrtc.weightedinstance import WeightedInstance
+
 
 class MAX_SMTI_IP(LpProblem):
+    """An IP model to find a maximum cardinality stable matching.
+    """
 
     def __init__(self, instance, name=None):
         if instance.number_of_couples_left() != 0:
@@ -23,6 +27,25 @@ class MAX_SMTI_IP(LpProblem):
         self._variables_lr = None
         self._variables_rl = None
 
+        self._use_dummy = False
+        """Maps the y variables, the dummy binary variables from Chapter 5 of paper.
+        """
+        self._variables_yl = None
+        self._variables_yr = None
+
+    @property
+    def dummy_variables(self):
+        """Are dummy variables used in this model."""
+        return self._use_dummy
+
+    @dummy_variables.setter
+    def dummy_variables(self, choice):
+        """Enable (or disable) the use of dummy variables. Note that this only
+        makes sense before building or solving.
+        """
+        if self._built:
+            raise Exception("Cannot modify model after it is built")
+        self._use_dummy = choice
 
     def _make_variables(self):
         """Makes the variables
@@ -46,14 +69,66 @@ class MAX_SMTI_IP(LpProblem):
         """
         for left in self._instance.single_agents_left:
             self += (lpSum(self._variables_lr[left.ident].values()) <= 1,
-                     f"left.{left.ident}.capacity")
+                     f"left_{left.ident}_capacity")
         for right in self._instance.single_agents_right:
             # If right.ident isn't in here, then this right agent has no
             # preferences.
             if right.ident not in self._variables_rl:
                 continue
             self += (lpSum(self._variables_rl[right.ident].values()) <= 1,
-                     f"right.{right.ident}.capacity")
+                     f"right_{right.ident}_capacity")
+
+    def _make_dummy_stability_constraints(self):
+        """Makes a variable variable for each agent and each rank k in their
+        preferences, which will be 1 if and only if the agent is matched with
+        another agent of rank k or less.
+        """
+        if self._variables_yl is not None:
+            return
+        self._variables_yl = {}
+        self._variables_yr = {}
+        for left in self._instance.single_agents_left:
+            self._variables_yl[left.ident] = {}
+            name = f"y_l_{left.ident}_1"
+            self._variables_yl[left.ident][1] = LpVariable(name, cat='Binary')
+            sum_left = []
+            for right_id in left.preferences[0]:
+                sum_left.append(self._variables_lr[left.ident][right_id])
+            self += (lpSum(sum_left) == self._variables_yl[left.ident][1])
+            for k, tie_group in enumerate(left.preferences[1:], start=2):
+                name = f"y_l_{left.ident}_{k}"
+                self._variables_yl[left.ident][k] = LpVariable(name,
+                                                               cat='Binary')
+                sum_left = []
+                for right_id in tie_group:
+                    sum_left.append(self._variables_lr[left.ident][right_id])
+                self += (lpSum(sum_left) + self._variables_yl[left.ident][k-1]
+                         == self._variables_yl[left.ident][k])
+        for right in self._instance.single_agents_right:
+            self._variables_yr[right.ident] = {}
+            name = f"y_r_{right.ident}_1"
+            self._variables_yr[right.ident][1] = LpVariable(name, cat='Binary')
+            sum_right = []
+            for left_id in right.preferences[0]:
+                sum_right.append(self._variables_lr[left_id][right.ident])
+            self += (lpSum(sum_right) == self._variables_yr[right.ident][1])
+            for k, tie_group in enumerate(right.preferences[1:], start=2):
+                name = f"y_r_{right.ident}_{k}"
+                self._variables_yr[right.ident][k] = LpVariable(name,
+                                                                cat='Binary')
+                sum_right = []
+                for left_id in tie_group:
+                    sum_right.append(self._variables_lr[left_id][right.ident])
+                self += (lpSum(sum_right)
+                         + self._variables_yr[right.ident][k-1]
+                         == self._variables_yr[right.ident][k])
+        for left in self._instance.single_agents_left:
+            for k, tie_group in enumerate(left.preferences[1:], start=2):
+                for right_id in tie_group:
+                    right = self._instance.single_agent_right(right_id)
+                    rank = right.rank_of(left.ident)
+                    self += (1 - self._variables_yl[left.ident][k]
+                             <= self._variables_yr[right_id][rank])
 
     def _make_stability_constraints(self):
         """Makes the stability constraints.
@@ -61,11 +136,29 @@ class MAX_SMTI_IP(LpProblem):
         for left in self._instance.single_agents_left:
             for right_id in left.acceptable_agents():
                 right = self._instance.single_agent_right(right_id)
-                xiq = lpSum([self._variables_lr[left.ident][other]
-                             for other in left.as_good_as(right.ident)])
-                xpj = lpSum([self._variables_rl[right.ident][other]
-                             for other in right.as_good_as(left.ident)])
+                xiq = lpSum(self._variables_lr[left.ident][other]
+                            for other in left.as_good_as(right.ident))
+                xpj = lpSum(self._variables_rl[right.ident][other]
+                            for other in right.as_good_as(left.ident))
                 self += 1 - xiq <= xpj, f"{left.ident}.{right.ident}.stability"
+
+    def _make_dummy_objective(self):
+        """Make an objective based on the xij variables.
+        """
+        objective = []
+        for left in self._instance.single_agents_left:
+            max_group = len(left.preferences)
+            objective.append(self._variables_yl[left.ident][max_group])
+        self += lpSum(objective)
+
+    def _make_regular_objective(self):
+        """Make an objective based on the xij variables.
+        """
+        objective = []
+        for left in self._instance.single_agents_left:
+            for right in left.acceptable_agents():
+                objective.append(self._variables_lr[left.ident][right])
+        self += lpSum(objective)
 
     def build_model(self):
         """Build the model.
@@ -74,7 +167,12 @@ class MAX_SMTI_IP(LpProblem):
             return
         self._make_variables()
         self._make_capacity_constraints()
-        self._make_stability_constraints()
+        if self._use_dummy:
+            self._make_dummy_stability_constraints()
+            self._make_dummy_objective()
+        else:
+            self._make_stability_constraints()
+            self._make_regular_objective()
         self._built = True
 
     def solve(self):
