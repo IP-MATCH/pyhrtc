208782af64f4191b79581bdb472d530937937468
William Pettersson <william.pettersson@glasgow.ac.uk>
2019-08-12 20:39:19 +0100

    Add algorithm to calculate biggest antichain

--- pyhrtc/algorithms.py
+++ pyhrtc/algorithms.py
@@ -1,7 +1,7 @@
 """Contains some calls to algorithms from 3rd parties.
 """
 
-from networkx import Graph as NxGraph, connected_component_subgraphs
+from networkx import Graph as NxGraph, DiGraph, connected_component_subgraphs
 from networkx.algorithms.bipartite import maximum_matching
 from networkx.algorithms.components import is_connected
 from networkx.algorithms.matching import max_weight_matching as nx_max_weight
@@ -71,3 +71,50 @@ def max_weight_matching(instance):
         for start, end in nx_max_weight(component):
             weight += graph[start][end]['weight']
     return weight
+
+def largest_weight_antichain(agents):
+    """Given a set of agents, determine how complex their preferences are,
+    where complexity is defined as distance from a commonly shared strict
+    ordering of options.
+    """
+    # Step 1, determine possible relationships
+
+    # This class stores possible relationships. In particular, it says "is it
+    # possible for a to always be more preferred than b"
+    class Relation:
+        def __init__(self):
+            self.under = True
+            self.over = True
+    # Sets of possible relationships
+    relations = {}
+    for agent in agents:
+        found = set()
+        for group in agent.preferences:
+            for now in group:
+                for earlier in found:
+                    pair = [earlier, now]
+                    if pair not in relations:
+                        relations[pair] = Relation()
+                    relations[pair].under = False
+                    pair = [now, earlier]
+                    if pair not in relations:
+                        relations[pair] = Relation()
+                    relations[pair].over = False
+            for now in group:
+                found.add(now)
+    # Step 2, build the graph
+    graph = DiGraph()
+    for pair, relation in relations.items():
+        if not relation.over:
+            continue
+        # a can be over b!
+        a, b = pair
+        if a not in graph.nodes():
+            graph.add_node(a)
+        if b not in graph.nodes():
+            graph.add_node(b)
+        graph.add_edge(a, b)  # We're pointing "down" towards less preferred
+    # Steps 3 and 4
+    longest = max(len(antichain) for antichain in graph.condensation())
+    return longest
+
