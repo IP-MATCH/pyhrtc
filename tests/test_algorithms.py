84b5e467c2cd18e534402809562e909eb78ae039
William Pettersson <william.pettersson@glasgow.ac.uk>
2024-11-11 14:11:40 +0000

    Updates after 5 years of no maintenance
    Mostly moving to pyproject.toml, dropping nosetests and moving to
    pytest, plus other small things

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,7 +1,6 @@
 """Test cases for maximum size matching algorithms."""
 
-
-from nose.tools import eq_, raises
+import pytest
 
 from pyhrtc.fileio import read_hrtc
 from pyhrtc.algorithms import max_card_matching, max_weight_matching
@@ -11,43 +10,43 @@ def test_max_card_matching_simple():
     """test1 is a simple instance of SMTI.
     """
     instance = read_hrtc("tests/testfiles/test1.instance")
-    eq_(max_card_matching(instance), 2)
+    assert max_card_matching(instance) == 2
 
 
 def test_max_card_matching_smti():
     """This is a much larger SMTI instance.
     """
     instance = read_hrtc("tests/testfiles/smti.instance")
-    eq_(max_card_matching(instance), 9941)
+    assert max_card_matching(instance) == 9941
 
 
 def test_max_card_matching_hrt():
     """An instance with capacities.
     """
     instance = read_hrtc("tests/testfiles/hrt.instance")
-    eq_(max_card_matching(instance), 759)
+    assert max_card_matching(instance) == 759
 
 
-@raises(Exception)
 def test_max_card_matching_hrct():
     """An instance with couples. Note that this isn't implemented.
     """
     instance = read_hrtc("tests/testfiles/test2.instance")
-    eq_(max_card_matching(instance), -1)
+    with pytest.raises(Exception):
+        assert max_card_matching(instance) == -1
 
 def test_max_card_matching_smti_grp():
     """This is a simple instance of SMTI-GRP.
     """
     instance = read_hrtc("tests/testfiles/smti-grp-simple.instance")
-    eq_(max_card_matching(instance), 3)
+    assert max_card_matching(instance) == 3
 
 
 def test_max_weight_matching():
     """A simple SMTI-GRP instance.
     """
     instance = read_hrtc("tests/testfiles/smti-grp-simple.instance")
-    eq_(max_weight_matching(instance), 200)
+    assert max_weight_matching(instance) == 200
     instance.threshold(30)
-    eq_(max_weight_matching(instance), 197)
+    assert max_weight_matching(instance) == 197
     instance.threshold(50)
-    eq_(max_weight_matching(instance), 177)
+    assert max_weight_matching(instance) == 177
--- tests/test_readers.py
+++ tests/test_readers.py
@@ -1,8 +1,6 @@
 """Test cases for reading files."""
 
 
-from nose.tools import eq_
-
 from pyhrtc.fileio import read_hrtc
 
 
@@ -10,46 +8,46 @@ def test_reads():
     """Read in test1.instance and test2.instance and check some parameters.
     """
     instance = read_hrtc("tests/testfiles/test1.instance")
-    eq_(instance.number_of_single_agents_left(), 2)
-    eq_(instance.number_of_couples_left(), 0)
-    eq_(instance.number_of_single_agents_right(), 2)
+    assert instance.number_of_single_agents_left() == 2
+    assert instance.number_of_couples_left() == 0
+    assert instance.number_of_single_agents_right() == 2
 
     instance = read_hrtc("tests/testfiles/test2.instance")
-    eq_(instance.number_of_single_agents_left(), 2)
-    eq_(instance.number_of_couples_left(), 2)
-    eq_(instance.number_of_single_agents_right(), 2)
+    assert instance.number_of_single_agents_left() == 2
+    assert instance.number_of_couples_left() == 2
+    assert instance.number_of_single_agents_right() == 2
 
 
 def test_reads_smti_grp():
     """Test reading of SMTI-GRP instances."""
     # With column/row headers
     instance = read_hrtc("tests/testfiles/smti-grp-simple.instance")
-    eq_(instance.number_of_single_agents_left(), 3)
-    eq_(instance.single_agent_left("1").weight_of("1"), 48)
-    eq_(instance.single_agent_right("2").weight_of("3"), 55)
-    eq_(instance.number_of_single_agents_right(), 3)
+    assert instance.number_of_single_agents_left() == 3
+    assert instance.single_agent_left("1").weight_of("1") == 48
+    assert instance.single_agent_right("2").weight_of("3") == 55
+    assert instance.number_of_single_agents_right() == 3
     # Without the header row/column, but a header showing number of rows and
     # columns instead
     instance = read_hrtc("tests/testfiles/smti-grp-noheader.instance")
-    eq_(instance.number_of_single_agents_left(), 3)
-    eq_(instance.single_agent_left("1").weight_of("1"), 48)
-    eq_(instance.single_agent_right("2").weight_of("3"), 55)
-    eq_(instance.number_of_single_agents_right(), 3)
+    assert instance.number_of_single_agents_left() == 3
+    assert instance.single_agent_left("1").weight_of("1") == 48
+    assert instance.single_agent_right("2").weight_of("3") == 55
+    assert instance.number_of_single_agents_right() == 3
     # An unbalanced instance.
     instance = read_hrtc("tests/testfiles/smti-grp-noheader-unbal.instance")
-    eq_(instance.number_of_single_agents_left(), 2)
-    eq_(instance.single_agent_left("1").weight_of("1"), 48)
-    eq_(instance.single_agent_right("3").weight_of("2"), 94)
-    eq_(instance.number_of_single_agents_right(), 3)
+    assert instance.number_of_single_agents_left() == 2
+    assert instance.single_agent_left("1").weight_of("1") == 48
+    assert instance.single_agent_right("3").weight_of("2") == 94
+    assert instance.number_of_single_agents_right() == 3
 
 
 def test_reads_iain():
     """Test reading of SMTI-GRP instances."""
     instance = read_hrtc("tests/testfiles/hrtc-iain-small.instance")
-    eq_(instance.number_of_single_agents_left(), 6)
-    eq_(instance.number_of_couples_left(), 2)
-    eq_(instance.number_of_single_agents_right(), 5)
+    assert instance.number_of_single_agents_left() == 6
+    assert instance.number_of_couples_left() == 2
+    assert instance.number_of_single_agents_right() == 5
     instance = read_hrtc("tests/testfiles/hrtc-iain.instance")
-    eq_(instance.number_of_single_agents_left(), 88)
-    eq_(instance.number_of_couples_left(), 11)
-    eq_(instance.number_of_single_agents_right(), 11)
+    assert instance.number_of_single_agents_left() == 88
+    assert instance.number_of_couples_left() == 11
+    assert instance.number_of_single_agents_right() == 11
